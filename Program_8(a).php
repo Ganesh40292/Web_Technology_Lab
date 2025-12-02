3c58b7a48f604a68142d46a378fd37c3d089cece
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2025-12-03 00:03:44 +0530

    Refactor visitor counter PHP code and styling

--- Program_8(a).php
+++ Program_8(a).php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <title>Visitor Counter</title>
- <style>
- body{ 
-    font-family:Arial,sans-serif; 
-    text-align:center; 
-    margin-top:50px; 
-    }
- h1{
-    color:#333; 
-    }
- p{ 
-    font-size:1.2em;
-    }
- </style>
+<meta charset="UTF-8">
+<title>Visitor Counter</title>
+
+<style>
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 50px;
+}
+h1 {
+    color: #333;
+}
+p {
+    font-size: 1.2em;
+}
+</style>
 </head>
+
 <body>
- <?php
- $file='Counter.txt';
- $count=file_exists($file)?(int)file_get_contents($file):0;
- file_put_contents($file,++$count);
- ?>
- 
- <h1> Visitor Counter </h1>
- <p>Number of Visitors: <strong><?php echo $count; ?></strong></p>
+
+<?php
+$file = "Counter.txt";
+
+if (!file_exists($file)) {
+    file_put_contents($file, 0);
+}
+
+$count = (int) file_get_contents($file);
+$count++;
+file_put_contents($file, $count);
+?>
+
+<h1>Visitor Counter</h1>
+<p>Number of Visitors: <strong><?php echo $count; ?></strong></p>
+
 </body>
 </html>
